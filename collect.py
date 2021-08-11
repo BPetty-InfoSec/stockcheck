e3e73f3ccf3fc883bc8b867609bec9312592bfa3
BPetty-InfoSec <bwpetty@protonmail.com>
2021-08-11 08:17:55 -0400

    Check the value of a stock Fixes #1

--- collect.py
+++ collect.py
@@ -1,10 +1,22 @@
-import json
-import pandas as pd
-import numpy as np
-import yfinance as yf
-from yahoofinancials import YahooFinancials
-from datetime import datetime, date, timedelta
+#!/usr/bin/python3
 
-ticker = yf.Ticker('AAPL')
-aapl_df = ticker.history(period="max")
-aapl_df['Close'].plot(title="AAPL's Stock Price")
+import json
+import yfinance as yf
+
+# Grab the stocks to check from the "stocks.json" file
+stocks_file = open("stocks.json",)
+stocks_list = json.load(stocks_file)
+stocks_file.close()
+stock_names = []
+stock_symbols = []
+# Extract the stock names and symbols into separate lists
+for item in stocks_list['ToCheck']:
+    stock_names.append(item)
+    stock_symbols.append(stocks_list['ToCheck'][item])
+
+# Grab stock info from each symbol
+for item in stock_symbols:
+    stock_info = yf.Ticker(item)
+    print(stock_info.info)
+
+
