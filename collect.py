df13346fbf59797af201eddeec4acf22602be5ce
BPetty-InfoSec <bwpetty@protonmail.com>
2021-08-11 21:56:04 -0400

    Save stock info to json file in order to continue working with info after retrieval Fixes #4

--- collect.py
+++ collect.py
@@ -4,19 +4,17 @@ import json
 import yfinance as yf
 
 # Grab the stocks to check from the "stocks.json" file
-stocks_file = open("stocks.json",)
-stocks_list = json.load(stocks_file)
+stocks_file = open("stocks.json",'r')
+stock_data = json.load(stocks_file)
 stocks_file.close()
-stock_names = []
-stock_symbols = []
-# Extract the stock names and symbols into separate lists
-for item in stocks_list['ToCheck']:
-    stock_names.append(item)
-    stock_symbols.append(stocks_list['ToCheck'][item])
 
 # Grab stock info from each symbol
-for item in stock_symbols:
-    stock_info = yf.Ticker(item)
-    print(stock_info.info)
+for item in stock_data['ToCheck']:
+    print("Gathering information on " + item + "...")
+    stock_info = yf.Ticker(stock_data['ToCheck'][item])
+    stock_data["Info"][item] = stock_info.info
 
+# Write info to json file
+stocks_file = open("stocks.json","w")
+json.dump(stock_data, stocks_file, indent=4)
 
